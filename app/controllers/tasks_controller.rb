841df9617a51b7c12a01bbaabc299805e1b4a678
shin <5446shin@gmail.com>
2019-02-17 17:36:36 +0900

    一覧画面、詳細画面、作成画面を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,16 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
     def index
-
+      @tasks = Task.all
     end
 
     def new
-
+      @task = Task.new
     end
 
     def create
-
+      Task.create!(task_params)
+      redirect_to tasks_path, notice: "タスクを作成いたしました"
     end
 
     def show
@@ -16,16 +18,25 @@ class TasksController < ApplicationController
     end
 
     def edit
-        
+
     end
-    
+
     def update
-        
+
     end
 
     def destroy
-        
+
+    end
+
+    private
+
+    def task_params
+      params.require(:task).permit(:title, :content, :priority, :limit, :status, :image, :user_id)
+    end
+
+    def set_task
+      @task = Task.find(params[:id])
     end
 
 end
-
