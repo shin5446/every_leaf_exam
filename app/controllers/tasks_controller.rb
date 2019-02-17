91eb605cff39f6db81dfcb4f7ebde08496970883
shin <5446shin@gmail.com>
2019-02-17 18:15:59 +0900

    編集画面、削除画面を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,7 @@ class TasksController < ApplicationController
     def create
       @task = Task.create(task_params)
       if @task.save
-      redirect_to tasks_path, notice: "タスクを作成いたしました"
+        redirect_to tasks_path, notice: "タスクを作成いたしました"
       else
         render 'new'
       end
@@ -26,11 +26,16 @@ class TasksController < ApplicationController
     end
 
     def update
-
+      if @task.update(task_params)
+        redirect_to tasks_path, notice: "タスクを編集しました！"
+      else
+        render 'edit'
+      end
     end
 
     def destroy
-
+      @task.destroy
+      redirect_to tasks_path, notice:"タスクを削除しました！"
     end
 
     private
