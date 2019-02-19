67dbbf4ea6499dfbe1b65247781ecfc4cfdd08d4
shin <5446shin@gmail.com>
2019-02-19 18:19:02 +0900

    コンフリクト解消

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module Everyleaf
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
 
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
