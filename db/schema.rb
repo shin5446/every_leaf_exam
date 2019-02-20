d9220dcdb33439c055013d8e52091ec254538334
shin <5446shin@gmail.com>
2019-02-20 18:12:20 +0900

    Taskモデルのtitleカラムにユニーク制約を設定、title,contentカラムにバリデーションを設定

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,16 @@
-ActiveRecord::Schema.define(version: 2019_02_15_052927) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_02_20_090321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_02_15_052927) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_tasks_on_title", unique: true
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
