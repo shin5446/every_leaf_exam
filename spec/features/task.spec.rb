5f77eb45fe7d15986f348eee610e58e8891ac3e1
shin <5446shin@gmail.com>
2019-02-22 14:58:34 +0900

    タスク終了期限作成のテスト,タスクが終了期限の降順に並んでいるかのテストを記述

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -1,6 +1,6 @@
 # このrequireで、Capybaraなどの、Feature Specに必要な機能を使用可能な状態にしています
 require 'rails_helper'
-
+require 'date'
 # このRSpec.featureの右側に、「タスク管理機能」のように、テスト項目の名称を書きます（do ~ endでグループ化されています）
 RSpec.feature "タスク管理機能", type: :feature do
   let(:task_a) {Task.create!(title: 'まいど', content: 'おおきに')}
@@ -39,6 +39,24 @@ RSpec.feature "タスク管理機能", type: :feature do
   scenario "タスクが作成日時の降順に並んでいるかのテスト" do
   visit tasks_path
   expect(Task.order("created_at DESC").map(&:id)).to eq [4,3,2,1]
-  save_and_open_page
+  # save_and_open_page
   end
+  scenario "タスク終了期限作成のテスト" do
+    visit new_task_path
+    fill_in 'task_title', with: 'あああ'
+    fill_in 'task[content]', with: 'いいい'
+    fill_in 'task_deadline', with: Date.today
+    click_on '登録する'
+    # save_and_open_page
+    expect(page).to have_content "以下の内容で、送信する。\nタイトル:あああ\n本文:いいい\n終了期限:2019-02-22\n優先順位:高\n状態:未"
+    click_on '登録する'
+  end
+
+  scenario "タスクが終了期限の降順に並んでいるかのテスト" do
+    visit tasks_path
+    click_link '終了期限でソートする'
+    # save_and_open_page
+    expect(Task.order("deadline DESC").map(&:title)).to eq ["test_task_04","test_task_02","test_task_03","test_task_01"]
+  end
+
 end
