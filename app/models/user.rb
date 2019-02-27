a14873bea4b3c33a7aeaabe37c55a9ef43bc537e
shin <5446shin@gmail.com>
2019-02-27 17:15:14 +0900

    Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :name, :email, presence: true
+    validates :name, length: { maximum: 30 }
+    validates :email, length: { maximum: 60 },uniqueness: true,
+                      format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
 end
