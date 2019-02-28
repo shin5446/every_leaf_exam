6f1ba1c956de7f15afb4f310bb7eb19252259274
shin <5446shin@gmail.com>
2019-02-28 12:54:52 +0900

    ログインログアウト機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  resources :users, only: [:new, :show, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
 end
