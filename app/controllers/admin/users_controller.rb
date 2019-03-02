fd160f8c26ee0e844ef360bf676b69a71f5564d0
shin <5446shin@gmail.com>
2019-03-02 14:21:29 +0900

    管理ユーザー作成、詳細、編集、機能を実装

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,55 @@
+class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザーを登録しました"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.new(user_params)
+
+    if @user.update
+      redirect_to admin_users_path, notice: "ユーザーを更新しました"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path, nitice: "ユーザーを削除しました"
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password,:password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
+
   get '/', to: 'tasks#index'
   resources :tasks do
     collection do
       post :confirm
     end
   end
+
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users
+  resources :users,except: [:index]
 end
