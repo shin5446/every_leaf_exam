c6234a53ea795e1f92229ab2fa1b6f61e2c15701
shin <5446shin@gmail.com>
2019-03-02 18:04:36 +0900

    ユーザが持っているタスクの数を表示するように実装、N+1問題を回避するためロジックをUserコントローラーに記述

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @users = User.all
+    @users = User.includes(:tasks).all
   end
 
   def show
