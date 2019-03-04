f15bf1f19a172bec7b14d1a151baf60597409bd5
shin <5446shin@gmail.com>
2019-03-04 12:10:22 +0900

    Task,Label,TAsk_labelモデルに関連づけを記述

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,14 +1,16 @@
 class Task < ApplicationRecord
-  enum priority: [:低, :中, :高]
-  belongs_to :user
-  validates :title, :content, presence: true
-  validates :title, uniqueness: true,length: { minimum: 1, maximum: 30 }
-  validates :content,length: { minimum: 1, maximum: 600 }
+    enum priority: [:低, :中, :高]
+    belongs_to :user
+    has_many :task_labels, dependent: :destroy
+    has_many :labels, through: :article_categories
+    validates :title, :content, presence: true
+    validates :title, uniqueness: true,length: { minimum: 1, maximum: 30 }
+    validates :content,length: { minimum: 1, maximum: 600 }
 
-scope :sort_deadline, -> {order(deadline: :desc)}
-scope :sort_priority, -> {order(priority: :desc)}
-scope :sort_created_at, -> {order(created_at: :desc)}
-# scope :search_title_status, -> (a,b){where("title LIKE ? and status LIKE ?" , "%#{ a }%", "%#{ b }%")}
-scope :search_title, -> (a){where("title LIKE ?", "%#{ a }%")}
-scope :search_status, -> (a){where("status LIKE ?", "%#{ a }%")}
+    scope :sort_deadline, -> {order(deadline: :desc)}
+    scope :sort_priority, -> {order(priority: :desc)}
+    scope :sort_created_at, -> {order(created_at: :desc)}
+    # scope :search_title_status, -> (a,b){where("title LIKE ? and status LIKE ?" , "%#{ a }%", "%#{ b }%")}
+    scope :search_title, -> (a){where("title LIKE ?", "%#{ a }%")}
+    scope :search_status, -> (a){where("status LIKE ?", "%#{ a }%")}
 end
