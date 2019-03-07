52bc71e265f191bf3c87f6dc44bc0f00645a0565
shin <5446shin@gmail.com>
2019-03-07 16:56:27 +0900

    管理ユーザー画面からユーザーを編集するときにパスワードがからでも編集できるように実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     before_validation { email.downcase! }
     has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
+    validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
     before_update  :no_edit_last_one
     before_destroy :at_least_one_admin
 
