41c1075c3fe71a58f73a1470ec2826c6609829d7
shin <5446shin@gmail.com>
2019-03-08 21:12:13 +0900

    priorityを空で登録するとエラーになるので、コールバックを設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
-    enum priority: [:低, :中, :高]
+    # enum priority: [:" " , :低, :中, :高]
+    # enum priority: [:低, :中, :高]
+    enum priority: {" "=> 0, "低"=> 1, "中"=> 2, "高"=> 3}
     belongs_to :user
     has_many :task_labels, dependent: :destroy
     has_many :labels, through: :task_labels, source: :label
@@ -11,7 +13,6 @@ class Task < ApplicationRecord
     validates :title, :content, presence: true
     validates :title, uniqueness: true,length: { minimum: 1, maximum: 30 }
     validates :content,length: { minimum: 1, maximum: 600 }
-    validates :priority, presence: true, allow_nil: true
 
     scope :sort_deadline, -> {order(deadline: :desc)}
     scope :sort_priority, -> {order(priority: :desc)}
@@ -20,4 +21,15 @@ class Task < ApplicationRecord
     scope :search_title, -> (a){where("title LIKE ?", "%#{ a }%")}
     scope :search_status, -> (a){where("status LIKE ?", "%#{ a }%")}
     scope :search_label, -> (a){where(id: label_ids = TaskLabel.where(label_id: a).select(:task_id))}
+
+    before_save :skip_null_false
+
+    private
+
+    def skip_null_false
+
+        if self.priority == nil
+            self.priority = 0
+        end
+    end
 end
